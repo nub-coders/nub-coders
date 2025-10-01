1a59b13ba907a5e8c43686a5a81583f484b4e866
NUB CODER <dev@nub-coder.tech>
2025-10-01 18:06:12 +0530

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,19 +1,3 @@
-# Copy the configuration files to sites-available
-sudo cp nub-coder.tech /etc/nginx/sites-available/
-sudo cp build.nub-coder.tech /etc/nginx/sites-available/
-sudo cp dockers.nub-coder.tech /etc/nginx/sites-available/
-sudo cp api.nub-coder.tech /etc/nginx/sites-available/
-
-
-
-# Create symbolic links in sites-enabled
-sudo ln -s /etc/nginx/sites-available/nub-coder.tech /etc/nginx/sites-enabled/
-sudo ln -s /etc/nginx/sites-available/build.nub-coder.tech /etc/nginx/sites-enabled/
-sudo ln -s /etc/nginx/sites-available/dockers.nub-coder.tech /etc/nginx/sites-enabled/
-sudo ln -s /etc/nginx/sites-available/api.nub-coder.tech /etc/nginx/sites-enabled/
-
-
-
 # Install Certbot if not already installed
 sudo apt update
 sudo apt install certbot python3-certbot-nginx
